c052ba08bd6b073a19af8b99264a78fb0c5bedb3
zxhaaa6 <zxhaaa@hotmail.com>
2018-10-27 15:32:07 +0800

    feat: support convert to pdf | html | Buffer with options

--- test/convert.js
+++ test/convert.js
@@ -1,14 +1,47 @@
 const path = require('path');
 const unoconv = require('../dist');
+const fs = require('fs');
 
-const input = path.resolve('tmp/1.docx');
-const output = path.resolve('tmp/1.pdf');
+const inputPath = path.resolve('tmp/a.docx');
+const outputPath = path.resolve('tmp/1.pdf');
 
-unoconv
-  .convert(input, output)
-  .then(result => {
-    console.log(result);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// #1
+function testDirectly() {
+  unoconv
+    .convert(inputPath, outputPath)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+// #2
+function testOptions() {
+  unoconv
+    .convert(inputPath, { output: outputPath, format: 'pdf' })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+// #3
+function testBuffer() {
+  unoconv
+    .convert(inputPath, { buffer: true, format: 'pdf' })
+    .then(buffer => {
+      fs.writeFileSync(outputPath, buffer, { encoding: 'utf8' });
+      console.log(`File save at ${outputPath}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+// testDirectly();
+// testOptions();
+// testBuffer();
